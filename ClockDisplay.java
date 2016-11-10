2f80221d99c49fe5b7bdedd0ae38e7866fd07b7e
Oscar Charro Rivera <oschariv@gmail.com>
2016-11-10 11:50:04 +0100

    clases creadas, metodo setTiempo creado

---
+++ ClockDisplay.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @oschariv (Oscar Charro Rivera) 
+ * @1.0 (10/11/2016)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private NumberDisplay hora;
+    private NumberDisplay min;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        hora = new NumberDisplay(24);
+        min = new NumberDisplay(60);
+    }
+
+    /**
+     * Metodo para establecer una hora determinada al ClockDisplay.
+     */
+    public void setTiempo(int horas, int minutos)
+    {
+        hora.setValue(horas);
+        min.setValue(minutos);
+    }
+}
